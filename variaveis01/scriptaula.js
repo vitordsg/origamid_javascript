d7efd92c1cdf699b20e583a37f621201e3176760
vitordsg <vitinho6.6@hotmail.com>
2023-06-03 02:28:10 -0300

    variaveis

---
+++ variaveis01/scriptaula.js
@@ -0,0 +1,39 @@
+var nome = 'Vitor Santos'
+var idade = 24
+var possuFaculdade = true
+console.log(nome, idade, possuFaculdade)
+
+
+
+var preço = 5
+var totalCompra = 25
+var total = preço * totalCompra
+console.log(total)
+
+
+
+var sobrenome = 'Vitor', 
+    cidade = 'SV'
+
+console.log(sobrenome, cidade)
+
+
+var semDefinir
+console.log(semDefinir)
+
+
+//console.log(aindaNaoDefinido)
+
+
+var comida
+console.log(comida)
+comida = 'Pizza'
+console.log(comida)
+
+//const time = 'Vasco'
+//time = 'Flamengo'
+
+//console.log(time)
+
+
+
---
+++ variaveis01/scriptex01.js
@@ -0,0 +1,24 @@
+// EXERCICIOS
+
+// Declarar uma variável com o seu nome
+var nome = 'Vitor'
+
+// Declarar uma variável com a sua idade
+var idade = 24
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var Temaki = ''
+
+// Atribuir valor a sua comida favorita
+var Temaki = '10'
+
+// Declarar 5 variáveis diferentes sem valores
+var l = 'L',
+    o = 'O',
+    v = 'V',
+    e = 'E',
+    d = 'D'
+
+console.log(nome, idade,Temaki,l,o,v,e,d)
+
