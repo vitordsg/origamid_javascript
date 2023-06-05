43d943fda822dc392649ffe5b1ea9db0230a0072
vitordsg <vitinho6.6@hotmail.com>
2023-06-04 22:59:44 -0300

    tipos de dados

---
+++ tiposdedadados/scriptaula02.js
@@ -0,0 +1,31 @@
+var idade = 23
+var simbolo = Symbol()
+console.log(typeof simbolo)
+
+
+var nome = 'Vitor'
+var sobrenome = 'Santos'
+var nomeCompleto = nome + sobrenome
+console.log(nomeCompleto)
+
+
+
+var gols = 1000
+var frase = 'Vitor fez ' + gols + ' gols'
+console.log(typeof frase)
+
+
+
+var ano = '2023'
+var mes = 8
+console.log(ano + mes)
+
+
+var melhor = 'teste'
+var frase1 = "Esse é o \"melhor\" jogo"
+console.log(frase1)
+
+var gols1 = 1000
+var frase3 = `Vitor fez ${gols1 * 2} gols`
+
+console.log(frase3)
---
+++ tiposdedadados/scriptex02.js
@@ -0,0 +1,30 @@
+// Declare uma variável contendo uma string
+var nome = 'Vitor2'
+console.log(nome)
+
+
+// Declare uma variável contendo um número dentro de uma string
+var idade = '24'
+console.log(typeof idade)
+
+
+// Declare uma variável com a sua idade
+var idade2 = 24
+console.log(idade2)
+
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var primeiro = 'Vitor'
+var segundo = 'Santos'
+var nomeCompleto = ` ${primeiro} ${segundo}`
+console.log(nomeCompleto)
+
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's Time"
+console.log(frase)
+
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome)
