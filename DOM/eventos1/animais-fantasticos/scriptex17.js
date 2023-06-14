ac37605a7907bb7edbe968e260145abd587a7542
vitordsg <vitinho6.6@hotmail.com>
2023-06-14 01:01:16 -0300

    att

--- DOM/eventos1/animais-fantasticos/scriptex17.js
+++ DOM/eventos1/animais-fantasticos/scriptex17.js
@@ -1,7 +1,27 @@
 // Duplique o menu e adicione ele em copy
+const menu = document.querySelector('.menu')
+const rodape = document.querySelector('.copy')
+
+const menuClone = menu.cloneNode(true)
+rodape.appendChild(menuClone)
+
+
 
 // Selecione o primeiro DT da dl de Faq
+const faq = document.querySelector('.faq-lista')
+console.log(faq.children[0])
+
+//const faq = document.querySelector('.faq')
+//const primeiroDt = faq.querySelector('dt')
+
 
 // Selecione o DD referente ao primeiro DT
+console.log(faq.children[0].nextElementSibling)
+//const proximoDD = primeiroDt.nextElementSibling
+//console.log(proximoDD)
+
 
 // Substitua o conteúdo html de .faq pelo de .animais
+const animais = document.querySelector('.animais')
+
+faq.innerHTML = animais.innerHTML
