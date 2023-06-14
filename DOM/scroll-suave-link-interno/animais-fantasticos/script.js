fdf61a66b042686e3eb65a3e1490a34aa76ad8c5
vitordsg <vitinho6.6@hotmail.com>
2023-06-14 20:45:24 -0300

    scroll suave link interno

---
+++ DOM/scroll-suave-link-interno/animais-fantasticos/script.js
@@ -0,0 +1,49 @@
+function initiationNavTab() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li')
+    const tabContent = document.querySelectorAll('.js-tabcontent section')
+
+    if(tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo')
+
+        function activeTab(index){
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo')
+            })
+            tabContent[index].classList.add('ativo')
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index)
+            })
+        })
+    }
+}
+
+initiationNavTab()
+
+
+
+
+
+
+function initiationAccordionList(){
+    const accordionList = document.querySelectorAll('.js-accordion dt')
+    const activeClass = 'ativo'
+
+    if(accordionList.length){
+        accordionList[0].classList.add(activeClass)
+        accordionList[0].nextElementSibling.classList.add(activeClass)
+
+        function activeAccordion(event) {
+            this.classList.toggle(activeClass)
+            this.nextElementSibling.classList.toggle(activeClass)
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion)
+        })
+    }
+}
+
+initiationAccordionList()
