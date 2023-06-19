5cf817ec5dd02f4b9d470a5b6b0ff877e28c5786
vitordsg <vitinho6.6@hotmail.com>
2023-06-19 19:10:25 -0300

    native host e user1

--- native-host-user1/scriptexercicio.js
+++ native-host-user1/scriptexercicio.js
@@ -1,35 +1,38 @@
 // Liste 5 objetos nativos
-Object
-String
-Array
-Function
-Boolean
-Number
-Math
-RegExp
-Map
-Set
+// Object
+// String
+// Array
+// Function
+// Boolean
+// Number
+// Math
+// RegExp
+// Map
+// Set
 
 
 
 
 // Liste 5 objetos do browser
-NodeList
-HTMLCollection
-Element
-document
-window
-navigator
-localStorage
-XMLHttpRequest
+// NodeList
+// HTMLCollection
+// Element
+// document
+// Window
+// history
+// window
+// navigator
+// localStorage
+// XMLHttpRequest
 
 
 
 
 // Liste 2 Métodos, Propriedades ou Objetos
 // presentes no Chrome que não existem no Firefox
-chrome.extension
-chrome.devtools
-chrome.notifications
-chrome.tabs.captureVisibleTab
-chrome.identity
+if (typeof document.webkitVisibilityState !== "undefined") {
+  console.log('Existe')
+}
+else {
+  console.log('Não Existe')
+}
