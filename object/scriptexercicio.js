71ac91e9b74dc59ffec0fd694b778f172958d575
vitordsg <vitinho6.6@hotmail.com>
2023-06-30 23:31:01 -0300

    Object

--- object/scriptexercicio.js
+++ object/scriptexercicio.js
@@ -1,9 +1,33 @@
 // Crie uma função que verifique
 // corretamente o tipo de dado
 
+verificarDados = function(dado) {
+    return Object.prototype.toString.call(dado)
+}
+
+console.log(verificarDados(''))
+
+
+
+
 // Crie um objeto quadrado com
 // a propriedade lados e torne
 // ela imutável
+const quadrado = {}
+
+Object.defineProperties(quadrado , {
+    lados: {
+        value: 4,
+        enumerable: true
+    }
+})
+
+console.log(quadrado)
+
+
+
+
+
 
 // Previna qualquer mudança
 // no objeto abaixo
@@ -12,8 +36,17 @@ const configuracao = {
     height: 600,
     background: '#333'
   }
+Object.freeze(configuracao)
   
+
+
+
+
   // Liste o nome de todas
   // as propriedades do
   // protótipo de String e Array
+
+
+console.log(Object.getOwnPropertyNames(String.prototype))
+console.log(Object.getOwnPropertyNames(Array.prototype))
   
