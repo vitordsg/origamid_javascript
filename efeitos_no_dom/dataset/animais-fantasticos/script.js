91bc09e7484d5fa83380b610953c604421034bcc
vitordsg <vitinho6.6@hotmail.com>
2023-07-03 20:53:22 -0300

    dataset

--- efeitos_no_dom/dataset/animais-fantasticos/script.js
+++ efeitos_no_dom/dataset/animais-fantasticos/script.js
@@ -1,103 +1,12 @@
-function initiationNavTab() {
-    const tabMenu = document.querySelectorAll('.js-tabmenu li')
-    const tabContent = document.querySelectorAll('.js-tabcontent section')
+// HTMLELEMENT DATASET
 
-    if(tabMenu.length && tabContent.length) {
-        tabContent[0].classList.add('ativo')
+const div = document.querySelector('[data-cor]')
 
-        function activeTab(index){
-            tabContent.forEach((section) => {
-                section.classList.remove('ativo')
-            })
-            tabContent[index].classList.add('ativo')
-        }
+//console.log(Object.prototype.toString.call(div))
 
-        tabMenu.forEach((itemMenu, index) => {
-            itemMenu.addEventListener('click', () => {
-                activeTab(index)
-            })
-        })
-    }
-}
+div.dataset.height = 1000
+delete div.dataset.width
+div.dataset.totalHeight = 2000
 
-initiationNavTab()
+console.log(div.dataset)
 
-
-
-
-
-
-function initiationAccordionList(){
-    const accordionList = document.querySelectorAll('.js-accordion dt')
-    const activeClass = 'ativo'
-
-    if(accordionList.length){
-        accordionList[0].classList.add(activeClass)
-        accordionList[0].nextElementSibling.classList.add(activeClass)
-
-        function activeAccordion(event) {
-            this.classList.toggle(activeClass)
-            this.nextElementSibling.classList.toggle(activeClass)
-        }
-
-        accordionList.forEach((item) => {
-            item.addEventListener('click', activeAccordion)
-        })
-    }
-}
-
-initiationAccordionList()
-
-
-function initiationScroll() {
-    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
-
-    function scrollToSection(event) {
-        event.preventDefault()
-        const href = event.currentTarget.getAttribute('href')
-        const section = document.querySelector(href)
-
-        section.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-
-        })
-        
-        // Forma Alternativa
-        // const topo = section.offsetTop
-        // window.scrollTo({
-        //     top: topo,
-        //     behavior: 'smooth'
-        // })
-    }
-
-    linksInternos.forEach((link) => {
-        link.addEventListener('click', scrollToSection)
-    })
-}
-
-initiationScroll()
-
-function initiationAnime(){
-    const sections = document.querySelectorAll('.js-scroll')
-    if(sections.length) {
-        const windowMetade = window.innerHeight * 0.6
-
-        function animaScroll(){
-            sections.forEach((section) =>{
-                const sectionTop = section.getBoundingClientRect().top
-                const isSectionVisible = (sectionTop - windowMetade) < 0
-                if(isSectionVisible)
-                    section.classList.add('ativo')
-                else
-                    section.classList.remove('ativo')
-            })
-        }
-
-        animaScroll()
-
-        window.addEventListener('scroll', animaScroll)
-    }
-}
-
-initiationAnime()
