29d65481ac8296c07efbccf0daede9209e509f6a
vitordsg <vitinho6.6@hotmail.com>
2023-07-03 23:03:01 -0300

    Modal

---
+++ efeitos_no_dom/modal/js/script.js
@@ -0,0 +1,14 @@
+import initiationScrollSuave from './modules/scroll-suave.js'
+import initiationAnimacaoScroll from './modules/scroll-animacao.js'
+import initiationAccordionList from './modules/accordion.js'
+import initiationNavTab from './modules/tabnav.js'
+import * as teste from './modules/teste.js'
+
+teste.teste1()
+teste.teste2()
+console.log(teste.senha)
+
+initiationScrollSuave()
+initiationAnimacaoScroll()
+initiationAccordionList()
+initiationNavTab()
