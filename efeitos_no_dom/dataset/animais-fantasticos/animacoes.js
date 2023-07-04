de0354bca856b9baa3b2eec43edd2698d87cab4e
vitordsg <vitinho6.6@hotmail.com>
2023-07-03 21:48:05 -0300

    dataset

--- efeitos_no_dom/dataset/animais-fantasticos/animacoes.js
+++ efeitos_no_dom/dataset/animais-fantasticos/animacoes.js
@@ -1,6 +1,6 @@
 function initiationNavTab() {
-    const tabMenu = document.querySelectorAll('.js-tabmenu li')
-    const tabContent = document.querySelectorAll('.js-tabcontent section')
+    const tabMenu = document.querySelectorAll('[data-tab="menu"] li')
+    const tabContent = document.querySelectorAll('[data-tab="content"] section')
 
     if(tabMenu.length && tabContent.length) {
         tabContent[0].classList.add('ativo')
@@ -9,7 +9,8 @@ function initiationNavTab() {
             tabContent.forEach((section) => {
                 section.classList.remove('ativo')
             })
-            tabContent[index].classList.add('ativo')
+            const direcao = tabContent[index].dataset.anime
+            tabContent[index].classList.add('ativo', direcao)
         }
 
         tabMenu.forEach((itemMenu, index) => {
@@ -28,7 +29,7 @@ initiationNavTab()
 
 
 function initiationAccordionList(){
-    const accordionList = document.querySelectorAll('.js-accordion dt')
+    const accordionList = document.querySelectorAll('[data-anime="accordion"] dt')
     const activeClass = 'ativo'
 
     if(accordionList.length){
@@ -50,7 +51,7 @@ initiationAccordionList()
 
 
 function initiationScroll() {
-    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+    const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]')
 
     function scrollToSection(event) {
         event.preventDefault()
@@ -79,7 +80,7 @@ function initiationScroll() {
 initiationScroll()
 
 function initiationAnime(){
-    const sections = document.querySelectorAll('.js-scroll')
+    const sections = document.querySelectorAll('[data-anime="scroll"]')
     if(sections.length) {
         const windowMetade = window.innerHeight * 0.6
 
