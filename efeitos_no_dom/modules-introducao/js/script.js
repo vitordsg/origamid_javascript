cf493b26ceb2f7d361a1c9698c704e11286c6fa2
vitordsg <vitinho6.6@hotmail.com>
2023-07-03 22:33:21 -0300

    modules

---
+++ efeitos_no_dom/modules-introducao/js/script.js
@@ -0,0 +1,9 @@
+import initiationScrollSuave from './modules/scroll-suave.js'
+import initiationAnimacaoScroll from './modules/scroll-animacao.js'
+import initiationAccordionList from './modules/accordion.js'
+import initiationNavTab from './modules/tabnav.js'
+
+initiationScrollSuave()
+initiationAnimacaoScroll()
+initiationAccordionList()
+initiationNavTab()
