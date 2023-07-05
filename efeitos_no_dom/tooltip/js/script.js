343fba46ddbd78b3a5ce062d3a0dff5cb2e7b1de
vitordsg <vitinho6.6@hotmail.com>
2023-07-05 20:39:04 -0300

    tooltip

---
+++ efeitos_no_dom/tooltip/js/script.js
@@ -0,0 +1,12 @@
+import initiationScrollSuave from './modules/scroll-suave.js'
+import initiationAnimacaoScroll from './modules/scroll-animacao.js'
+import initiationAccordionList from './modules/accordion.js'
+import initiationNavTab from './modules/tabnav.js'
+import initModal from './modules/modal.js'
+
+
+initiationScrollSuave()
+initiationAnimacaoScroll()
+initiationAccordionList()
+initiationNavTab()
+initModal()
