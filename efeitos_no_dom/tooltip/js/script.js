f1a44fb5f88014031afe6199ada791d4a18a0cf6
vitordsg <vitinho6.6@hotmail.com>
2023-07-05 20:57:29 -0300

    tooltip

--- efeitos_no_dom/tooltip/js/script.js
+++ efeitos_no_dom/tooltip/js/script.js
@@ -3,6 +3,7 @@ import initiationAnimacaoScroll from './modules/scroll-animacao.js'
 import initiationAccordionList from './modules/accordion.js'
 import initiationNavTab from './modules/tabnav.js'
 import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
 
 
 initiationScrollSuave()
@@ -10,3 +11,4 @@ initiationAnimacaoScroll()
 initiationAccordionList()
 initiationNavTab()
 initModal()
+initTooltip()
