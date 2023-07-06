2c3790ae7572d4bdf1b8029a1baab3abb43f0813
vitordsg <vitinho6.6@hotmail.com>
2023-07-05 21:39:45 -0300

    dropdown menu

---
+++ efeitos_no_dom/dropdown-menu/js/script.js
@@ -0,0 +1,14 @@
+import initiationScrollSuave from './modules/scroll-suave.js'
+import initiationAnimacaoScroll from './modules/scroll-animacao.js'
+import initiationAccordionList from './modules/accordion.js'
+import initiationNavTab from './modules/tabnav.js'
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
+
+
+initiationScrollSuave()
+initiationAnimacaoScroll()
+initiationAccordionList()
+initiationNavTab()
+initModal()
+initTooltip()
