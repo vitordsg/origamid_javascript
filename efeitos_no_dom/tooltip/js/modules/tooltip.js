da12a031b2a65d947a1257dd37dca5320de82e27
vitordsg <vitinho6.6@hotmail.com>
2023-07-05 21:23:07 -0300

    tooltip

--- efeitos_no_dom/tooltip/js/modules/tooltip.js
+++ efeitos_no_dom/tooltip/js/modules/tooltip.js
@@ -9,12 +9,26 @@ tooltips.forEach((item) => {
 })
 
 function onMouseOver(event) {
-    criarTooltipBox(this)
-    console.log(event)
+    const tooltiBox = criarTooltipBox(this)
+    tooltiBox.style.top = event.pageY + 'px'
+    tooltiBox.style.left = event.pageX + 'px'
+
+    onMouseLeave.tooltipBox = tooltiBox
+    this.addEventListener('mouseleave', onMouseLeave)
+}
+
+const onMouseLeave = {
+    tooltipBox : '',
+    handleEvent() {
+        this.tooltipBox.remove()
+    }
 }
 
 function criarTooltipBox(element) {
     const tooltipBox = document.createElement('div')
     const text = element.getAttribute('aria-label')
     tooltipBox.classList.add('tooltip')
+    tooltipBox.innerText = text
+    document.body.appendChild(tooltipBox)
+    return tooltipBox
 }
