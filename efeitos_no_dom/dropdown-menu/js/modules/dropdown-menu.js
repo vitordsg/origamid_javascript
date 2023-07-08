ff5591bfcc77cef6ee515d5476e176edb086449a
vitordsg <vitinho6.6@hotmail.com>
2023-07-07 23:02:57 -0300

    dropdown menu

--- efeitos_no_dom/dropdown-menu/js/modules/dropdown-menu.js
+++ efeitos_no_dom/dropdown-menu/js/modules/dropdown-menu.js
@@ -2,3 +2,42 @@ export default function initDropdownMenu() {
 
 }
 
+const dropdownMenus = document.querySelectorAll('[data-dropdown]')
+
+dropdownMenus.forEach(menu => {
+    ['touchstart', 'click'].forEach(userEvent => {
+        menu.addEventListener(userEvent, handleClick)
+    })
+})
+
+function handleClick(event) {
+    event.preventDefault()
+    this.classList.add('active')
+    outsideClick(this, ['touchstart', 'click'], () => {
+        this.classList.remove('active')
+    })
+}
+
+function outsideClick(element, events, callback) {
+    const html = document.documentElement
+    const outside = 'data-outside'
+
+    if(!element.hasAttribute(outside)) {
+        events.forEach(userEvent => {
+            html.addEventListener(userEvent, handleOutSideClick)
+        })
+        element.setAttribute(outside, '')
+    }
+
+
+    function handleOutSideClick(event) {
+        
+        if(!element.contains(event.target)) {
+            element.removeAttribute(outside)
+            events.forEach(userEvent => {
+                html.removeEventListener(userEvent, handleOutSideClick)
+            })
+            callback()
+        }
+    }
+}
