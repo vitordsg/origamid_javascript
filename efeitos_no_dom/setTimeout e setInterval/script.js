43922a6244cd42d52d066413534e322d41e6e4cf
vitordsg <vitinho6.6@hotmail.com>
2023-07-10 19:08:04 -0300

    setTimeout e setInterval

--- efeitos_no_dom/setTimeout e setInterval/script.js
+++ efeitos_no_dom/setTimeout e setInterval/script.js
@@ -9,3 +9,56 @@ setTimeout(() => {
 
 
 
+// IMEDIATO
+setTimeout(() => {
+ console.log('Agora')   
+});
+
+
+
+
+// LOOPS E SETTIMEOUT
+// for(let i = 0; i <= 20; i++) {
+//     setTimeout(() => {
+//         console.log(i)
+//     }, 300 * i)
+// }
+
+
+
+// THIS E WINDOW
+const btn = document.querySelector('button')
+
+btn.addEventListener('click', handleCLick)
+
+function handleCLick() {
+    setTimeout(() => {
+        console.log(this)
+        this.classList.add('active')
+    }, 1000);
+}
+
+
+
+
+// SETINTERVAL
+// function loop(texto) {
+//     console.log(texto)
+// }
+
+
+// let i = 0
+// setInterval(() => {
+//     console.log(i++)
+// }, 1000)
+
+
+
+// CLEARINTERVAL
+
+let a = 0
+const meuLoop = setInterval(() => {
+    console.log(a++)
+    if(a > 10)
+    clearInterval(meuLoop)
+}, 300)
