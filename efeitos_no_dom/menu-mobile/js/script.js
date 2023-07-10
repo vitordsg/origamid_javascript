6f0206fc6663967faca2727ad8191d382c4c61f7
vitordsg <vitinho6.6@hotmail.com>
2023-07-10 20:24:50 -0300

    menu mobile

---
+++ efeitos_no_dom/menu-mobile/js/script.js
@@ -0,0 +1,16 @@
+import initiationScrollSuave from './modules/scroll-suave.js'
+import initiationAnimacaoScroll from './modules/scroll-animacao.js'
+import initiationAccordionList from './modules/accordion.js'
+import initiationNavTab from './modules/tabnav.js'
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
+import initDropdownMenu from './modules/dropdown-menu.js'
+
+
+initiationScrollSuave()
+initiationAnimacaoScroll()
+initiationAccordionList()
+initiationNavTab()
+initModal()
+initTooltip()
+initDropdownMenu()
