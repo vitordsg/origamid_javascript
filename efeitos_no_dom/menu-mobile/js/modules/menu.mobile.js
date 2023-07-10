dda8e3e6ef40901d4a784f102d947c4ea5bd0eb5
vitordsg <vitinho6.6@hotmail.com>
2023-07-10 20:47:18 -0300

    menu mobile

---
+++ efeitos_no_dom/menu-mobile/js/modules/menu.mobile.js
@@ -0,0 +1,8 @@
+import outsideClick from './outsideclick.js'
+
+export default function initMenuMobile() {
+
+}
+
+const menuButton = document.querySelector('[data-menu="button"]')
+const menuList = document.querySelector('[data-menu="list"]')
