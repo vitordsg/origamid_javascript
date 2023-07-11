32500d9816053827c403fefb0d90cd23641d4c8b
vitordsg <vitinho6.6@hotmail.com>
2023-07-11 18:27:46 -0300

    anima numeros

---
+++ efeitos_no_dom/anima-numeros/js/script.js
@@ -0,0 +1,18 @@
+import initiationScrollSuave from './modules/scroll-suave.js'
+import initiationAnimacaoScroll from './modules/scroll-animacao.js'
+import initiationAccordionList from './modules/accordion.js'
+import initiationNavTab from './modules/tabnav.js'
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
+import initDropdownMenu from './modules/dropdown-menu.js'
+import initMenuMobile from './modules/menu.mobile.js'
+
+
+initiationScrollSuave()
+initiationAnimacaoScroll()
+initiationAccordionList()
+initiationNavTab()
+initModal()
+initTooltip()
+initDropdownMenu()
+initMenuMobile()
