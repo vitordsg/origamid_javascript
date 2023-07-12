f14ab62509f39794158757f15ac3de205f7a049e
vitordsg <vitinho6.6@hotmail.com>
2023-07-12 19:56:35 -0300

    data object

---
+++ efeitos_no_dom/date-object/js/modules/funcionamento.js
@@ -0,0 +1,19 @@
+export default function initFuncionamento() {
+    
+}
+
+// const agora = new Date()
+// const futuro = new Date('Dec 24 2023 23:59')
+
+// console.log(agora.getMonth())
+
+// console.log(futuro)
+
+// function transformarDias(tempo) {
+//     return tempo / (24 * 60 * 60 * 1000)
+// }
+
+// const diasAgora = transformarDias(agora.getTime())
+// const diasFuturo = transformarDias(futuro.getTime())
+
+// console.log(diasFuturo - diasAgora)
--- efeitos_no_dom/date-object/js/script.js
+++ efeitos_no_dom/date-object/js/script.js
@@ -7,6 +7,7 @@ import initTooltip from './modules/tooltip.js'
 import initDropdownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu.mobile.js'
 import initAnimaNumeros from './modules/anima-numeros.js'
+import initFuncionamento from './modules/funcionamento.js'
 
 
 initiationScrollSuave()
@@ -18,3 +19,4 @@ initTooltip()
 initDropdownMenu()
 initMenuMobile()
 initAnimaNumeros()
+initFuncionamento()
