695d34c6d3f31fcd8d135ee8a3c8eb6e75f3b7ed
vitordsg <vitinho6.6@hotmail.com>
2023-07-12 20:28:13 -0300

    data object

--- efeitos_no_dom/date-object/js/modules/funcionamento.js
+++ efeitos_no_dom/date-object/js/modules/funcionamento.js
@@ -2,6 +2,22 @@ export default function initFuncionamento() {
     
 }
 
+const funcionamento = document.querySelector('[data-semana]')
+const diasSemana = funcionamento.dataset.semana.split(',').map(Number)
+const horarioSemana = funcionamento.dataset.horario.split(',').map(Number)
+
+const dataAgora = new Date()
+const diaAgora = dataAgora.getDay()
+const horarioAgora = dataAgora.getHours()
+
+const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1])
+
+
+if(semanaAberto && horarioAberto) {
+    funcionamento.classList.add('aberto')
+}
+
 // const agora = new Date()
 // const futuro = new Date('Dec 24 2023 23:59')
 
