3959a9e4b1f30fea6ccc2befe6e5ff0f440cf402
vitordsg <vitinho6.6@hotmail.com>
2023-07-18 02:57:50 -0300

    Fetch

--- javascript-assicrono/fetch/script.js
+++ javascript-assicrono/fetch/script.js
@@ -20,3 +20,19 @@ corBackground.then(r => r.text())
     style.innerHTML = body
     cor.appendChild(style)
 })
+
+
+
+// HTML E .TEXT()
+
+const sobre = fetch('./sobre.html')
+const div = document.createElement('div')
+
+
+sobre.then(r => r.text())
+.then(body => {
+    div.innerHTML = body
+    const titulo = div.querySelector('h1')
+    document.querySelector('h1').innerText = titulo.innerText
+    console.log(titulo)
+})
