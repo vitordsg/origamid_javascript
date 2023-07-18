500dea3c8ebff5f8d823922bfbfb8c3ca002b7e1
vitordsg <vitinho6.6@hotmail.com>
2023-07-17 22:18:05 -0300

    Promise

--- javascript-assicrono/promises/script.js
+++ javascript-assicrono/promises/script.js
@@ -14,11 +14,50 @@ const promesa = new Promise((resolve, reject) => {
 
 
 
+
 // THEN
-promesa.then(function(resolution) {
-    console.log(resolution)
+// promesa.then(function(resolution) {
+//     console.log(resolution)
+// })
+
+// promesa.then(resolution => console.log(resolution))
+
+
+
+
+
+// ASSÍCRONO
+const promesa1 = new Promise((resolve, reject) => {
+    let condicao = false
+
+    if(condicao) {
+        setTimeout(() => {
+            resolve({nome: 'Vitor Santos', idade: 24})
+        }, 1000);
+    }
+
+    else {
+     reject(Error('Ocorreu um erro, tente novamente'))
+    }
 })
 
-promesa.then(function(resolution) {
-    console.log(resolution)
+promesa1.then(resolution1 => console.log(resolution1))
+
+
+
+
+// THEN().THEN()
+const retorno = promesa1
+
+.then(resolution1 => {
+    resolution1.profissao = 'Developer'
+    return resolution1
 })
+.then(resolution1 => {
+    console.log(resolution1) 
+
+}).catch(rejeitada => {
+    console.log(rejeitada)
+})
+
+console.log(retorno)
