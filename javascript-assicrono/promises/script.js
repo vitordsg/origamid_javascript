214f138489e3b1d96a10b5ae8e20cee17ca2fbeb
vitordsg <vitinho6.6@hotmail.com>
2023-07-17 22:31:18 -0300

    Promise

--- javascript-assicrono/promises/script.js
+++ javascript-assicrono/promises/script.js
@@ -28,7 +28,7 @@ const promesa = new Promise((resolve, reject) => {
 
 // ASSÍCRONO
 const promesa1 = new Promise((resolve, reject) => {
-    let condicao = false
+    let condicao = true
 
     if(condicao) {
         setTimeout(() => {
@@ -41,23 +41,57 @@ const promesa1 = new Promise((resolve, reject) => {
     }
 })
 
-promesa1.then(resolution1 => console.log(resolution1))
+//promesa1.then(resolution1 => console.log(resolution1))
 
 
 
 
-// THEN().THEN()
-const retorno = promesa1
+// THEN().THEN().CATCH()
+// const retorno = promesa1
 
-.then(resolution1 => {
-    resolution1.profissao = 'Developer'
-    return resolution1
+// .then(resolution1 => {
+//     resolution1.profissao = 'Developer'
+//     return resolution1
+// })
+// .then(resolution1 => {
+//     console.log(resolution1) 
+
+// }, rejeitada => {
+//     console.log(rejeitada)
+// }).finally(() => {
+//     console.log('Acabou')
+// })
+
+// console.log(retorno)
+
+
+
+
+// PROMISE.ALL()
+const login = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('Usuário logado')
+    }, 1000)
 })
-.then(resolution1 => {
-    console.log(resolution1) 
 
-}).catch(rejeitada => {
-    console.log(rejeitada)
+const dados = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('Dados Carregados')
+    }, 1500)
 })
 
-console.log(retorno)
+const carregouTudo = Promise.all([login, dados])
+
+carregouTudo.then((resolucao) => {
+    console.log(resolucao)
+})
+
+
+
+
+// PROMISE.RACE()
+const carregouTudo2 = Promise.race([login, dados])
+
+carregouTudo2.then((resolucao) => {
+    console.log(resolucao)
+})
