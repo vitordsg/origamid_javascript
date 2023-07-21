2e7382b5dce921644a286efe8c1903f507a68918
vitordsg <vitinho6.6@hotmail.com>
2023-07-20 21:37:01 -0300

    History API

---
+++ javascript-assicrono/History API/script.js
@@ -0,0 +1,47 @@
+// // PUSHSTATE
+// window.history.pushState(null, null, 'sobre.html')
+
+
+
+// // POPSTATE
+// window.addEventListener('popstate', () => {
+//     console.log('teste')
+// })
+
+
+
+// FETCH E HISTORY
+const links = document.querySelectorAll('a')
+
+function handleClick(event) {
+    event.preventDefault()
+    fetchPage(event.target.href)
+    window.history.pushState(null, null, event.target.href)
+}
+
+async function fetchPage(url) {
+    const pageResponse = await fetch(url)
+    const pageText = await pageResponse.text()
+    replaceContent(pageText)
+}
+
+function replaceContent(newText) {
+    const newHtml = document.createElement('div')
+    newHtml.innerHTML = newText
+
+    const oldContent = document.querySelector('.content')
+    const newContent = newHtml.querySelector('.content')
+
+    oldContent.innerHTML = newContent.innerHTML
+    document.title = newHtml.querySelector('title').innerText
+    console.log(newHtml)
+}
+
+window.addEventListener('popstate', () => {
+    
+    fetchPage(window.location.href)
+})
+
+links.forEach(link => {
+    link.addEventListener('click', handleClick)
+})
