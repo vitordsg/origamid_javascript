c3bfa7e42e7e5989b288fa76f031a10eabd19773
vitordsg <vitinho6.6@hotmail.com>
2023-07-20 21:57:52 -0300

    Animais Fetch

---
+++ javascript-assicrono/animais-fetch/js/script.js
@@ -0,0 +1,22 @@
+import initiationScrollSuave from './modules/scroll-suave.js'
+import initiationAnimacaoScroll from './modules/scroll-animacao.js'
+import initiationAccordionList from './modules/accordion.js'
+import initiationNavTab from './modules/tabnav.js'
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
+import initDropdownMenu from './modules/dropdown-menu.js'
+import initMenuMobile from './modules/menu.mobile.js'
+import initAnimaNumeros from './modules/anima-numeros.js'
+import initFuncionamento from './modules/funcionamento.js'
+
+
+initiationScrollSuave()
+initiationAnimacaoScroll()
+initiationAccordionList()
+initiationNavTab()
+initModal()
+initTooltip()
+initDropdownMenu()
+initMenuMobile()
+initAnimaNumeros()
+initFuncionamento()
