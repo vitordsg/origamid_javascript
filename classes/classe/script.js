929d90ac897deb07cbfc9a2f0b5f4845795b8fe4
vitordsg <vitinho6.6@hotmail.com>
2023-07-24 20:51:00 -0300

    class

--- classes/classe/script.js
+++ classes/classe/script.js
@@ -13,22 +13,73 @@
 // }
 
 
-// CLASS
+
+
+
+
+
+
+// // CLASS  // CONSTRUCTOR  //  RETURN  // THIS
 
 class Button {
-    constructor(text, background) {
+    constructor(text, background, color) {
         this.text = text
         this.background = background
+        this.color = color
     }
 
     element() {
         const buttonElement = document.createElement('button')
         buttonElement.innerText = this.text
         buttonElement.style.background = this.background
+        buttonElement.style.color = this.color
         return buttonElement
+    }
+
+    appendTo(target) {
+        const targetElement = document.querySelector(target)
+        targetElement.appendChild(this.element())
+        return targetElement    
+    }
+
+    static blueButton(text) {
+        return new Button(text, 'blue', 'white')
     }
 }
 
-const blueButton = new Button('Comprar', 'blue')
+const botaoAzul = Button.blueButton('Comprar ae')
 
-console.log(blueButton)
+// console.log(blueButton.appendTo('body'))
+
+
+
+
+
+// PROPRIEDADES
+
+// class Button {
+//     constructor(options) {
+//         this.options = options
+//     }
+
+//     static createButton(text, background) {
+//         const buttonElement = document.createElement('button')
+//         buttonElement.innerText = text
+//         buttonElement.style.background = background
+//         return buttonElement
+//     }
+// }
+
+// const optionsBlue = {
+//     backgroundColor: 'blue',
+//     color: 'white',
+//     text: 'Vem Comprar'
+
+// }
+
+
+// const blueBotton = new Button(optionsBlue)
+
+// const blueButtonStatic = Button.createButton('Clique', 'blue')
+
+// console.log(blueButtonStatic)
