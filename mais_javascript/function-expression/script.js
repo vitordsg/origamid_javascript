6e29a09bcd776b9cb8db923f0690409eff895284
vitordsg <vitinho6.6@hotmail.com>
2023-07-26 21:46:32 -0300

    function expression

---
+++ mais_javascript/function-expression/script.js
@@ -0,0 +1,25 @@
+// FUNCTION DECLARATION / EXPRESSION
+
+// function somar(a,b) {
+//     return a + b
+// }
+
+const somar = function(a,b) {
+    return a + b
+}
+
+
+
+
+// HOISTING
+console.log(somar(4,6))
+// console.log(dividir(2,2)) | ERRO
+
+
+function somar2(a,b) {
+    return a + b
+}
+
+const dividir = function(a,b) {
+    return a / b
+}
