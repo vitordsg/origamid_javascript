11dcb99f076199a173f2c50bfc87e6130b4d793b
vitordsg <vitinho6.6@hotmail.com>
2023-07-26 22:18:55 -0300

    Function Expression

--- mais_javascript/function-expression/script.js
+++ mais_javascript/function-expression/script.js
@@ -12,7 +12,8 @@ const somar = function(a,b) {
 
 
 // HOISTING
-console.log(somar(4,6))
+
+//console.log(somar(4,6))
 // console.log(dividir(2,2)) | ERRO
 
 
@@ -23,3 +24,38 @@ function somar2(a,b) {
 const dividir = function(a,b) {
     return a / b
 }
+
+
+
+// ARROW FUNCTION
+
+const somar3 = (a,b) => a + b
+const quadrado = a => a * a
+
+console.log(somar3(4,4))
+console.log(quadrado(4))
+
+
+// ESTRUTURA / PREFERÊNCIA
+
+function dividir2(a,b) {
+    return a / b
+}
+
+const dividir3 = (a,b) => a / b
+
+dividir2(4,2)
+dividir3(4,2)
+
+
+
+// IIFE - IMMEDIATELY INVOKED FUNCTION EXPRESSION  ||  IIFE - ARROW FUNCTION
+
+const instrumento = 'Violão';
+
+(() => {
+    const instrumento = 'Guitarra'
+    console.log(instrumento)
+})()
+
+console.log(instrumento)
---
+++ mais_javascript/function-expression/scriptsexercicio.js
@@ -0,0 +1,23 @@
+// Remova o erro
+const priceNumber = n => +n.replace('R$', '').replace(',', '.');
+priceNumber('R$ 99,99');
+
+
+
+
+// Crie uma IIFE e isole o escopo
+// de qualquer código JS.
+(function() {
+    const comida = 'Temaki';
+    console.log(comida)
+})()
+
+
+
+// Como podemos utilizar
+// a função abaixo.
+const active = callback => callback();
+
+active(function() {
+    console.log('Teste')
+})
