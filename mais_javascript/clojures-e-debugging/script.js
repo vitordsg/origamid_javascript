66966a296b08145afacaa05b731b8b27f0ea3276
vitordsg <vitinho6.6@hotmail.com>
2023-07-27 22:46:21 -0300

    Clojures e Debugging

--- mais_javascript/clojures-e-debugging/script.js
+++ mais_javascript/clojures-e-debugging/script.js
@@ -1,15 +1,30 @@
 //ESCOPO 
-let item1 = 1
-function funcao1() {
-    let item2 = 2
-    function funcao2() {
-        let item3 = 3
-        console.log(item1)
-        console.log(item2)
-        console.log(item3)
-    }
+// let item1 = 1
+// function funcao1() {
+//     let item2 = 2
+//     function funcao2() {
+//         let item3 = 3
+//         console.log(item1)
+//         console.log(item2)
+//         console.log(item3)
+//     }
 
-    funcao2()
+//     funcao2()
+// }
+
+// funcao1()
+
+
+
+//CASO CLÁSSICO
+function contagem() {
+    let total = 0
+    
+    return function incremento() {
+        total++
+        console.log(total)
+    }
 }
 
-funcao1()
+const ativarIncremento = contagem()
+ativarIncremento()
