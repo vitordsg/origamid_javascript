9e873a2b0587682ca45e67a712538337ca32e8bc
vitordsg <vitinho6.6@hotmail.com>
2023-08-02 22:16:37 -0300

    Regexp Métodos

--- regular-expression/regexp-metodos/script.js
+++ regular-expression/regexp-metodos/script.js
@@ -49,3 +49,100 @@ let regexpResult
 while((regexpResult = regexp3.exec(frase3)) !== null) {
     console.log(regexpResult[0])
 }
+
+
+
+
+
+
+
+
+// STR.MATCH()
+const resultados = frase3.match(regexp3)
+
+console.log(resultados)
+
+
+
+
+
+
+
+
+
+// STR.SPLIT()
+const frase4 = 'JavaScript, TypeScript, CoffeScript, Java, 200'
+
+const regexp4 = /[A-Za-z]+/g
+
+const resultados2 = frase4.split(regexp4)
+
+console.log(resultados2)
+
+
+
+
+
+
+
+
+
+// STR.REPLACE()
+const tags = `
+<ul>
+  <li>Item 1</li>
+  <li>Item 2</li>
+</ul>
+`;
+
+const regexp5 = /<li/g
+
+const resultado5 = tags.replace(regexp5, '$& class="ativa"')
+console.log(resultado5)
+
+
+
+
+
+
+
+
+// GRUPOS DE CAPTURA
+const emails = `
+empresa@email.com
+contato@email.com
+suporte@email.com
+`;
+
+const regexp6 = /(\w+@)[\w.]+/g
+
+const result = emails.replace(regexp6, '$1gmail.com')
+console.log(result)
+
+
+
+
+
+
+
+
+// CALLBACK
+const regexp7 = /(\w+@)([\w.]+)/g;
+const emails2 = `joao@homail.com.br
+marta@ggmail.com.br
+bruna@oulook.com.br`
+
+const resultado3 = emails2.replace(regexp7, function(...args) {
+    
+    if(args[2] === 'homail.com.br') {
+        return args[1] + 'hotmail.com.br'
+    } else if(args[2] === 'ggmail.com.br') {
+        return args[1] + 'gmail.com.br'
+    } else if(args[2] === 'oulook.com.br') {
+        return args[1] + 'outlook.com.br'
+    } else {
+    return args[0]
+    }
+})
+
+console.log(resultado3)
