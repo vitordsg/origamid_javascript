c7135215f7106a6e791fd4e60aab4d8bb795a9b4
vitordsg <vitinho6.6@hotmail.com>
2023-08-09 19:50:44 -0300

    Babel

---
+++ automacao-front-end/babel/js/script.js
@@ -0,0 +1,23 @@
+import initiationScrollSuave from './modules/scroll-suave.js';
+import initiationAnimacaoScroll from './modules/scroll-animacao.js';
+import initiationAccordionList from './modules/accordion.js';
+import initiationNavTab from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu.mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
+import iniFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
+
+initiationScrollSuave();
+initiationAnimacaoScroll();
+initiationAccordionList();
+initiationNavTab();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+iniFetchAnimais();
+initFetchBitcoin();
